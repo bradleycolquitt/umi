408bb1c82f014e3814e528fbb1eaefd17b3e01e5
lyre <bradley.colquitt@gmail.com>
2015-01-11 23:49:51 -0800

    started work on bam_sql.cpp

---
+++ src/bam_sql.cpp
@@ -0,0 +1,111 @@
+#include <htslib/sam.h>
+#include <sqlite3>
+
+using namespace std;
+// Fill class
+class BamDB {
+
+    // Variables
+    const char* bam_fname;
+    // bam_file bam;
+    bam_hdr_t* header;
+    hts_idx_t* idx;
+    unsigned int bam_counts;
+    const char* db_fname;
+    ppdb* conn;
+    vector<string> barcodes;
+
+
+    BamDB() : {};
+
+
+    //fill_db
+    vector<string>* get_barcodes() {
+        return barcodes;
+    }
+};
+
+int create_table(BamDB& bamdb) {
+    const char* statement = "CREATE TABLE IF NOT EXISTS align (name text,
+                                                           instrument text,
+                                                           flowcell text,
+                                                           chrom int,
+                                                           position int,
+                                                           strand int,
+                                                           bc int,
+                                                           umi text);";
+    sqlite3_exec(bamdb->conn, statement);
+    return 0;
+}
+
+int fill_db(BamDB& bamdb) {
+    boost::regex pattern = "[IDN]";
+    int tid = 0;
+    hts_itr_t* bam_itr = bam_itr_queryi(bamdb->idx, tid, 0, bamdb->refs[tid]);
+    bam1_t r;
+
+    uint32_t* cigar;
+    uint8_t* aux_p;
+    char* aux;
+
+    string qname;
+    int use = 1;
+
+    uint32_t pos;
+    while (bam_itr_next(bamdb->bam->fp.bgzf, bam_itr, r)) {
+
+        // get cigar
+        cigar = bam_get_cigar(r);
+
+        for (int i=0; i<r->core.n_cigar; ++i) {
+            uint32_t op = bam_cigar_op(cigar[i]);
+            if (op > 0 & op < 4) {
+                use = 0;
+                break;
+            }
+        }
+        if (!use) continue;
+
+        // get aux. filter out NH>1
+        aux_p = bam_get_aux(r);
+        aux = bam_aux2A(aux_p);
+        size_t aux_len = strlen(aux);
+
+        boost::regex pattern = "(NH\\:[0-9]+)";
+        boost::match_results<string::const_iterator> what;
+        while(boost::regex_search(aux, what, pattern, flags)) {
+            char* match = what[0];
+            if (strlen(match) > 4) {
+                use = 0;
+                break;
+            }
+            int mm = atoi(match[3]);
+            if (mm > 1) {
+                use = 0;
+                break;
+            }
+        }
+        if (!use) continue;
+
+        qname = *bam_get_qname(r);
+        vector<string> qname_list = split(qname); // need to include from R project
+        instrument = qname_list[0];
+        flowcell = qname_list[2];
+
+        if (bam_is_rev(r)) {
+            pos = bam_endpos(r);
+        } else {
+            pos = bam->core.pos;
+        }
+
+        seq_p = bam_get_seq(r);
+        qual_p = bam_get_qual(r);
+
+        bc = get_barcode(seq_p, qual_p, 5, 11, bamdb->barcodes);
+        umi = get_umi(seq_p, qual_p, 0, 4);
+    }
+
+    return 0;
+}
+
+int get_barcode(uint32_t)
