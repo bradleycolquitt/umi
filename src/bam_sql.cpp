6ceba0bc2aabd2a3845f9ce2625747a0186af1fe
lyre <bradley.colquitt@gmail.com>
2015-04-06 00:23:38 -0700

    major overhaul, initial attempt at adding PE info and a rejiggering of class structure

--- src/bam_sql.cpp
+++ src/bam_sql.cpp
@@ -5,15 +5,15 @@
 #include <string_utils.h>
 #include <boost/regex.hpp>
 #include <gperftools/profiler.h>
-//#include <seqan/sequence.h>
 //#define DEBUG
 
 using namespace std;
 
-BamDB::BamDB(const char* bam_fname, const char* dest_fname, const char* barcodes_fname, int umi_length, int bc_min_qual)
+BamDB::BamDB(const char* bam_fname, const char* dest_fname, const char* barcodes_fname, int umi_length, int bc_min_qual, bool paired_end)
     : bam_fname(bam_fname)
     , dest_fname(dest_fname)
     , bc_min_qual(bc_min_qual)
+    , paired_end(paired_end)
     {
         bam = sam_open(bam_fname, "rb");
         header = sam_hdr_read(bam);
@@ -41,16 +41,31 @@ BamDB::BamDB(const char* bam_fname, const char* dest_fname, const char* barcodes
             cout << e.what() << endl;
         }
 
+        //Positions of UMI and barcodes relative to 5' end of read
         sequence_pos.push_back(0);
         sequence_pos.push_back(umi_length-1);
         sequence_pos.push_back(umi_length);
-
         size_t bc_length = barcodes[0].size();
         sequence_pos.push_back(sequence_pos[2] + bc_length);
 
         int offsets_array[] = {0,-1,1}; // defines offsets used during barcode search
         bc_offsets.assign(offsets_array, offsets_array + sizeof(offsets_array) / sizeof(int));
+        // setup record
+        if (!paired_end) {
+            record = new dbRecordSe();
+        } else {
+            record = new dbRecordPe();
+        }
 
+        // prepare sqlite statements
+        const char* tail = 0;
+        char SQL[BUFFER_SIZE];
+        if (!paired_end) {
+        sprintf(SQL, "INSERT INTO align VALUES (@IN, @FL, @CL, @TID, @HPOS, @TPOS, @STR, @BC, @UMI);");
+        } else {
+        sprintf(SQL, "INSERT INTO align VALUES (@IN, @FL, @CL, @TID, @HPOS1, @TPOS1, @HPOS2, @HPOS2, @INS, @STR, @BC, @UMI);");
+        }
+        sqlite3_prepare_v2(conn, SQL, BUFFER_SIZE, &stmt, &tail);
     }
 
 // Read in barcodes file and load sequences into barcodes vector
@@ -92,7 +107,9 @@ void BamDB::create_reftable() {
 int create_table(BamDB* bamdb) {
     char* err_msg = 0;
     int rc;
-    const char* statement = "CREATE TABLE IF NOT EXISTS align (                      \
+    const char* statement;
+    if (!bamdb->is_pe()) {
+        statement = "CREATE TABLE IF NOT EXISTS align (                      \
                                                            instrument text,          \
                                                            flowcell text,            \
                                                            cluster text,             \
@@ -102,7 +119,22 @@ int create_table(BamDB* bamdb) {
                                                            strand int,               \
                                                            bc int,                   \
                                                            umi int);";
+    } else {
+        statement = "CREATE TABLE IF NOT EXISTS align (                      \
+                    instrument text,          \
+                    flowcell text,            \
+                    cluster text,             \
+                    tid int,                  \
+                    hpos1 int,                 \
+                    tpos1 int,                 \
+                    hpos2 int,                 \
+                    tpos2 int,                 \
+                    insert int,               \
+                    strand int,               \
+                    bc int,                   \
+                    umi int);";
 
+    }
     rc = sqlite3_exec(bamdb->get_conn(), statement, NULL, NULL, &err_msg);
     if( rc != SQLITE_OK ){
        fprintf(stderr, "SQL error: %s\n", err_msg);
@@ -120,20 +152,39 @@ int create_index(BamDB* bamdb) {
     return 0;
 }
 
-int insert_to_db(dbRecord* record, sqlite3_stmt* stmt) {
-
-    sqlite3_bind_text(stmt, 1, record->instrument, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_text(stmt, 2, record->flowcell, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_text(stmt, 3, record->cluster, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_int(stmt, 4, record->tid);
-    sqlite3_bind_int(stmt, 5, record->pos_head);
-    sqlite3_bind_int(stmt, 6, record->pos_tail);
-    sqlite3_bind_int(stmt, 7, record->strand);
-    sqlite3_bind_int(stmt, 8, record->bc);
-    sqlite3_bind_int(stmt, 9, record->umi);
+int dbRecordSe::insert_to_db(sqlite3_stmt* stmt) {
+    sqlite3_bind_text(stmt, 1, instrument, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 2, flowcell, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 3, cluster, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_int(stmt, 4, tid);
+    sqlite3_bind_int(stmt, 5, read_pos[0]);
+    sqlite3_bind_int(stmt, 6, read_pos[1]);
+    sqlite3_bind_int(stmt, 7, strand);
+    sqlite3_bind_int(stmt, 8, bc);
+    sqlite3_bind_int(stmt, 9, umi);
 
     sqlite3_step(stmt);
+    sqlite3_clear_bindings(stmt);
+    sqlite3_reset(stmt);
 
+    return 0;
+}
+
+int dbRecordPe::insert_to_db(sqlite3_stmt* stmt) {
+    sqlite3_bind_text(stmt, 1, instrument, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 2, flowcell, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 3, cluster, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_int(stmt, 4, tid);
+    sqlite3_bind_int(stmt, 5, read_pos[0]);
+    sqlite3_bind_int(stmt, 6, read_pos[1]);
+    sqlite3_bind_int(stmt, 7, read_pos[2]);
+    sqlite3_bind_int(stmt, 8, read_pos[3]);
+    sqlite3_bind_int(stmt, 9, insert);
+    sqlite3_bind_int(stmt, 10, strand);
+    sqlite3_bind_int(stmt, 11, bc);
+    sqlite3_bind_int(stmt, 12, umi);
+
+    sqlite3_step(stmt);
     sqlite3_clear_bindings(stmt);
     sqlite3_reset(stmt);
 
@@ -143,15 +194,12 @@ int insert_to_db(dbRecord* record, sqlite3_stmt* stmt) {
 int bad_cigar(bam1_t* b) {
     uint32_t* cigar = bam_get_cigar(b);
     int op;
-
     for (int i=0; i < b->core.n_cigar; ++i) {
         op = bam_cigar_op(cigar[i]);
-
         if (op == BAM_CINS || op == BAM_CDEL || op == BAM_CREF_SKIP) {
             return 1;
         }
     }
-
     return 0;
 }
 
@@ -168,25 +216,28 @@ int filter_multi_reads(bam1_t* b) {
 }
 
 // Split qname/read metadata
-void split_qname(bam1_t* b, dbRecord* record) {
+void dbRecord::split_qname(bam1_t* b) {
     char* qname = bam_get_qname(b);
     char* qname_array = strtok(qname, ":");
     int j = 0;
     while (qname_array != NULL) {
+        // change these to settors
         if (j == 0) {
-            strcpy(record->instrument, qname_array);
+            strcpy(instrument, qname_array);
+           // record->seinstrument(qname_array);
         } else if (j == 2) {
-            strcpy(record->flowcell, qname_array);
+            strcpy(flowcell, qname_array);
+            //record->set_flowcell(qname_array);
         } else if (j== 3) {
-            strcpy(record->cluster, qname_array);
+            strcpy(cluster, qname_array);
+            //record->set_cluster(qname_array);
         } else if (j > 3) {
-            strcat(record->cluster, ":");
-            strcat(record->cluster, qname_array);
+            strcat(cluster, ":");
+            strcat(cluster, qname_array);
         }
         qname_array = strtok(NULL, ":");
         ++j;
     }
-
 }
 
 int compare_barcode_local(vector<vector<int> >::iterator bc_iter, uint8_t* seq, int start, int end) {
@@ -296,94 +347,92 @@ int get_sequence(bam1_t* b, int start, int end, int used_offset) {
     }
 }
 
+void dbRecord::set_bc(BamDB* bamdb, bam1_t* b, int* used_offset) {
+    bc = get_sequence(b, bamdb->sequence_pos[2], bamdb->sequence_pos[3], bamdb->barcodes, bc_offsets, bc_min_qual, used_offset);
+}
+
+void dbRecord::set_umi(bam1_t* b, int used_offset) {
+    umi = get_sequence(b, sequence_pos[0], sequence_pos[1], used_offset);
+}
+
+int process_read1(BamDB* bamdb, bam1_t* b) {
+    // continue if read has indels or skipped refernences
+    if (bad_cigar(b)) return 2;
+
+    // continue if mapped to more than one position
+    if (filter_multi_reads(b)) return 3;
+
+    bamdb->get_record()->split_qname(b);
+    bamdb->get_record()->set_positions(b, 1);
+
+    int used_offset;
+    // extract barcode
+    bamdb->get_record()->set_bc(b, &used_offset);
+    // extract umi
+    bamdb->get_record()->set_umi(b, used_offset);
+
+    // try {
+    //     if (!bamdb->is_pe()) {
+    //         insert_to_db(bamdb->get_record(), stmt);
+    //     } else {
+    //         insert_to_db2(bamdb->get_record(), stmt);
+    //     }
+    // } catch ( exception& e ) {
+    //     exit(1);
+    // }
+}
+
+void process_read2(BamDB* bamdb, bam1_t* b) {
+// set_positions
+bamdb->get_record()->set_positions(b, 2);
+// set_insert_size
+bamdb->get_record()->set_insert(b);
+}
+
 int fill_db(BamDB* bamdb) {
     char* err_msg = 0;
-
     //turn off synchronous writing to disk for increased insertion speed
     sqlite3_exec(bamdb->get_conn(), "PRAGMA synchronous = OFF", NULL, NULL, &err_msg);
 
     hts_itr_t* bam_itr;
     for (int tid = 0; tid < bamdb->get_header()->n_targets; ++tid) {
         bam_itr = bam_itr_queryi(bamdb->get_idx(), tid, 0, bamdb->get_rlen(tid));
-
         fill_db_tid(bamdb, tid, bam_itr);
     }
     sqlite3_close(bamdb->get_conn());
     return 0;
-
 }
 
 int fill_db_tid(BamDB* bamdb, int tid, hts_itr_t* bam_itr) {
-    dbRecord record = {};
-    record.tid = tid;
+    //dbRecord record;
+    bamdb->get_record()->tid = tid;
 
     bam1_t* b = bam_init1();
     int result;
     int i = 0;
 
-    sqlite3_stmt* stmt;
     char* err_msg = 0;
-    const char* tail = 0;
-    char SQL[BUFFER_SIZE];
+
 
     vector<int> sequence_pos = bamdb->get_sequence_pos();
 
-    sprintf(SQL, "INSERT INTO align VALUES (@IN, @FL, @CL, @TID, @HPOS, @TPOS, @STR, @BC, @UMI);");
-    sqlite3_prepare_v2(bamdb->get_conn(),  SQL, BUFFER_SIZE, &stmt, &tail);
-
+    // if (!bamdb->is_pe()) {
+    // sprintf(SQL, "INSERT INTO align VALUES (@IN, @FL, @CL, @TID, @HPOS, @TPOS, @STR, @BC, @UMI);");
+    // } else {
+    //     sprintf(SQL, "INSERT INTO align VALUES (@IN, @FL, @CL, @TID, @HPOS1, @TPOS1, @HPOS2, @HPOS2, @INS, @STR, @BC, @UMI);");
+    // }
+    // sqlite3_prepare_v2(bamdb->get_conn(),  SQL, BUFFER_SIZE, &stmt, &tail);
     sqlite3_exec(bamdb->get_conn(), "BEGIN TRANSACTION", NULL, NULL, &err_msg);
 
     while ((result = sam_itr_next(bamdb->get_bam(), bam_itr, b)) >= 0) {
         if (b->core.flag&BAM_FMUNMAP) continue;
-        //bamdb->increment_read();
-        // FIX ME:
-        // if (bamdb->get_read() % progress_size == 0) {
-        //      cout << (float)bamdb->get_read() / bamdb->get_mapped() * 100  << "%..." << flush;
-        // }
-        //if (i == 100) exit(1);
-        //cout << "test" << endl;
-        ++i;
-
-        // continue if read has indels or skipped refernences
-        if (bad_cigar(b)) continue;
-
-        // continue if mapped to more than one position
-        if (filter_multi_reads(b)) {
-            continue;
-        }
-
-        split_qname(b, &record);
-
-        if (bam_is_rev(b)) {
-            record.pos_tail = b->core.pos + 1;
-            record.pos_head = bam_endpos(b);
-            record.strand = true;
-        } else {
-            record.pos_head = b->core.pos + 1;
-            record.pos_tail = bam_endpos(b);
-            record.strand = false;
-        }
-        // +1 offset for comparison with 1-based indexing
-
-        int used_offset;
-        // extract barcode
-        record.bc = get_sequence(b, sequence_pos[2], sequence_pos[3], \
-                                    bamdb->get_barcodes(), \
-                                    bamdb->get_bc_offsets(), \
-                                    bamdb->get_bc_min_qual(), \
-                                    used_offset);
-
-        // extract umi
-        record.umi = get_sequence(b, sequence_pos[0], sequence_pos[1], used_offset);
-
-        try {
-            insert_to_db(&record, stmt);
-        } catch ( exception& e ) {
-            exit(1);
+        if (b->core.flag&BAM_FREAD1) {
+            process_read1(bamdb, b);
+        else if (bamdb->is_pe() && b->core.flag&BAM_FREAD2) {
+            process_read2(bamdb, b);
         }
     }
-
     sqlite3_exec(bamdb->get_conn(), "END TRANSACTION", NULL, NULL, &err_msg);
-
     return 0;
-}
+    }
+    }
--- src/bam_sql.h
+++ src/bam_sql.h
@@ -2,7 +2,7 @@
 #define BAM_SQL_H
 
 #include <vector>
-#include <string>
+#include <string.h>
 #include <sqlite3.h>
 #include <htslib/sam.h>
 
@@ -11,10 +11,15 @@ using namespace std;
 #define BC_PATH "/home/brad/lib/barcodes/"
 #define BUFFER_SIZE 256
 
+//class BamDB;
+
+
+
 class BamDB {
     private:
             const char* bam_fname;
             const char* dest_fname;
+            const bool paired_end;
             samFile* bam;
             bam_hdr_t* header;
             hts_idx_t* idx;
@@ -22,8 +27,11 @@ class BamDB {
             uint64_t total_mapped;
             uint64_t total_read;
 
+            dbRecord* record;
             const char* db_fname;
             sqlite3* conn;
+            sqlite3_stmt* stmt;
+
             vector<vector<int> > barcodes;
             vector<int> sequence_pos;
             vector<int> bc_offsets;
@@ -31,12 +39,13 @@ class BamDB {
             int bc_min_qual;
 
     public:
-            BamDB(const char* bam_fname, const char* dest_fname, const char* barcodes_fname, int umi_length, int bc_min_qual);
-            void set_barcodes(const char* fname, vector<vector<int> >& vec_p);
-            void create_reftable();
-            void increment_read() { ++total_read;}
-            //void update_percent() { percent += 5; }
+            BamDB(const char* bam_fname, const char* dest_fname, const char* barcodes_fname, int umi_length, int bc_min_qual, bool paired_end);
 
+            /* Settors */
+            void set_barcodes(const char* fname, vector<vector<int> >& vec_p);
+
+            /* Gettors */
+            bool is_pe() { return paired_end; }
             vector<int> get_sequence_pos() { return sequence_pos; }
             samFile* get_bam() { return bam; }
             bam_hdr_t* get_header() { return header; }
@@ -47,24 +56,120 @@ class BamDB {
             vector<vector<int> >* get_barcodes() { return &barcodes; }
             vector<int>* get_bc_offsets() { return &bc_offsets; }
             int get_rlen(int tid) { return header->target_len[tid]; }
-            sqlite3* get_conn() {return conn;}
-            int get_bc_min_qual() { return bc_min_qual; }
+            sqlite3* get_conn() { return conn; }
+            sqlite3_stmt* get_stmt() { return stmt; }
 
+            int get_bc_min_qual() { return bc_min_qual; }
+            dbRecord* get_record() { return record; }
+
+            /* Other */
+            void create_reftable();
+            void increment_read() { ++total_read;}
+            //void update_percent() { percent += 5; }
+    friend class dbRecord;
 };
 
-struct dbRecord {
+class dbRecord : public BamDB {
+    protected:
+    //BamDB* bamdb;
     char instrument[BUFFER_SIZE];
     char flowcell[BUFFER_SIZE];
     char cluster[BUFFER_SIZE];
-
-    int tid = 0;
-    uint32_t pos_head = 0;
-    uint32_t pos_tail;
+    int tid, bc, umi;
+    vector<uint32_t> read_pos;
     bool strand;
-    int bc;
-    int umi;
 
+public:
+//dbRecord(BamDB* bamdb)
+dbRecord()
+{
+//bamdb = bamdb;
 };
+
+/* Settors */
+void set_instrument(const char* input) { strcpy(instrument, input); }
+void set_flowcell(const char* input) { strcpy(flowcell, input); }
+void set_cluster(const char* input) { strcpy(cluster, input); }
+void set_tid(int tid) { tid = tid; }
+void set_bc(BamDB* bamdb, bam1_t* b, int* used_offset);
+void set_umi(bam1_t* b, int umi_start, int umi_end, int used_offset);
+
+/* Gettors */
+//const char* get_instrument() { return instrument; }
+vector<uint32_t> get_pos() { return read_pos; }
+virtual int set_positions(bam1_t* b, int read_num);
+virtual int insert_to_db(sqlite3_stmt* stmt);
+void split_qname(bam1_t* b);
+};
+
+
+class dbRecordSe: public dbRecord {
+public:
+//dbRecordSe(BamDB* bamdb)
+dbRecordSe()
+{
+//dbRecord::bamdb = bamdb;
+read_pos.resize(2);
+};
+int set_positions(bam1_t* b, int read_num) {
+if (bam_is_rev(b)) {
+read_pos[1] = b->core.pos + 1;
+read_pos[0] = bam_endpos(b);
+strand = true;
+} else {
+read_pos[0] = b->core.pos + 1;
+read_pos[1] = bam_endpos(b);
+strand = false;
+}
+return 0;
+}
+/* Other */
+int insert_to_db(sqlite3_stmt* stmt);
+};
+
+class dbRecordPe: public dbRecord {
+protected:
+uint8_t insert;
+public:
+dbRecordPe() {
+//dbRecord::bamdb = bamdb;
+read_pos.resize(4);
+};
+// +1 offset for comparison with 1-based indexing
+int set_positions(bam1_t* b, int read_num) {
+if (read_num == 1) {
+if (bam_is_rev(b)) {
+read_pos[0] = bam_endpos(b);
+read_pos[1] = b->core.pos + 1;
+strand = true;
+} else {
+read_pos[0] = b->core.pos + 1;
+read_pos[1] = bam_endpos(b);
+strand = false;
+}
+
+} else if (read_num == 2) {
+if (bam_is_rev(b)) {
+read_pos[2] = bam_endpos(b);
+read_pos[3] = b->core.pos + 1;
+} else {
+read_pos[2] = b->core.pos + 1;
+read_pos[3] = bam_endpos(b);
+}
+
+}
+return 0;
+}
+
+void set_insert(bam1_t* b) {
+insert = b->core.isize;
+}
+
+/* Other */
+int insert_to_db(sqlite3_stmt* stmt);
+};
+
+
 
 int create_table(BamDB* bamdb);
 int create_index(BamDB* bamdb);
